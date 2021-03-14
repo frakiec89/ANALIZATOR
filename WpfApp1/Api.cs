2fc57e53d8e7ed205c63e4f6b7766cc0c7b4c1c8
РИШАТ Ахтямов <rishat-rus@mail.ru>
2021-03-15 00:30:04 +0400

    Добавьте файлы проекта.

---
+++ WpfApp1/Api.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Api
+    {
+        public static  string InPostAsync(ApiPost apiPost, string name)
+        {
+            WebRequest request = WebRequest.Create(String.Format("http://localhost:5000/api/analyzer/{0}", name));
+            request.Method = "POST"; // для отправки используется метод Post
+
+            // данные для отправки
+            string data = JsonConvert.SerializeObject(apiPost);
+            // преобразуем данные в массив байтов
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+            // устанавливаем тип содержимого - параметр ContentType
+            request.ContentType = "application/json";
+            // Устанавливаем заголовок Content-Length запроса - свойство ContentLength
+            request.ContentLength = byteArray.Length;
+
+            //записываем данные в поток запроса
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            WebResponse response =  request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    
+
+        public static ApiPost OutGEtAsync(string name)
+        {
+            WebRequest request = WebRequest.Create(String.Format("http://localhost:5000/api/analyzer/{0}", name));
+
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var content = reader.ReadToEnd();
+                    response.Close();
+                    return JsonConvert.DeserializeObject<ApiPost>(content);
+                }
+            }
+
+        }
+    }
+
+
+
+    public class ApiPost
+    {
+        public  string patient { get; set; }
+
+        public List<service> services { get; set; }
+
+    }
+
+
+    public class service
+    {
+        public int serviceCode { get; set; }
+        public int code { get; set; }
+
+        public string result { get; set; }
+
+    }
+
+    
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            a.Text = "Ledetect";
+            p.Text = "da293d99-95aa-4254-8c7e-c9e9270ef3f7";
+            s1.Text = "415";
+            s2.Text = "415";
+
+
+        }
+
+        private async void  b_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var servises = new List< service>
+                {
+                new service { serviceCode= Convert.ToInt32(s1.Text) },
+                new service { serviceCode= Convert.ToInt32(s2.Text) }
+                };
+
+                var inAnaliz = new ApiPost { patient = p.Text, services = servises };
+
+
+                var s = Api.InPostAsync(inAnaliz, a.Text);
+                MessageBox.Show(s);
+            }
+            catch (WebException ex)
+            {
+                WebResponse response = ex.Response;
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                      MessageBox.Show( reader.ReadToEnd());
+                    }
+                }
+
+            }
+           
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string content = string.Empty;
+
+                var s = Api.OutGEtAsync( a.Text);
+
+                var p = s.patient;
+                if (s != null && s.patient!=null && s.services!=null)
+                {
+                    foreach (var item in s.services)
+                    {
+                        content += p + " - " + "CoDe -" + item.code.ToString() + " status - " + item.result + "\n";
+
+                    }
+                    MessageBox.Show(content);
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse response = ex.Response;
+
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        MessageBox.Show(reader.ReadToEnd());
+                    }
+                }
+
+            }
+        }
+    }
+}
